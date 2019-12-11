1d4e8ab858afc999ef6d5d00dd06639e0d0bf21d
Hovo Nazaryan <hovnazaryan98@gmail.com>
2019-12-12 00:15:59 +0400

    Make carousel smooth

--- src/views/_partials/Home/Slider.vue
+++ src/views/_partials/Home/Slider.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="position-relative">
-        <carousel autoplay :per-page="1" :pagination-enabled="false" easing="ease" loop :autoplay-timeout="10000">
-            <slide>
-                <b-img src="/img/home/slider/slider-1.jpg" class="w-100"></b-img>
-            </slide>
-            <slide>
-                <b-img src="/img/home/slider/slider-2.jpg" class="w-100"></b-img>
-            </slide>
-        </carousel>
+        <section class="overflow-hidden">
+            <vue-flux
+                    :options="vfOptions"
+                    :images="vfImages"
+                    :transitions="vfTransitions"
+                    ref="slider">
+            </vue-flux>
+        </section>
 
-        <div class="w-100 position-absolute w-100 h-100 top-0">
+        <div class="w-100 position-absolute w-100 h-100 top-0 z-index-max">
             <b-container class="position-relative w-100 h-100">
                 <b-img src="/img/home/heroes.png" class="home-heroes d-none d-lg-block" v-if="!withoutHero"></b-img>
                 <quote-form :shown.sync="openQuote"></quote-form>
@@ -20,6 +20,7 @@
 
 <script>
     import QuoteForm from "../../../components/QuoteForm";
+    import { VueFlux } from 'vue-flux';
 
     export default {
         props: {
@@ -28,10 +29,16 @@
                 default: false
             }
         },
-        components: { QuoteForm },
+        components: { QuoteForm, VueFlux },
         data() {
             return {
-                openQuote: false
+                openQuote: false,
+                vfOptions: {
+                    autoplay: true,
+                    delay: 9000
+                },
+                vfImages: [ '/img/home/slider/slider-1.jpg', '/img/home/slider/slider-2.jpg'],
+                vfTransitions: [ 'fade' ]
             }
         },
         created() {
@@ -53,5 +60,24 @@
     left: 0;
     right: 0;
     margin: auto;
+}
+.z-index-max{
+    z-index: 980;
+}
+
+/deep/.vue-flux{
+    animation: zoominoutsinglefeatured 20s infinite;
+}
+
+@keyframes zoominoutsinglefeatured {
+    0% {
+        transform: scale(1,1);
+    }
+    50% {
+        transform: scale(1.2,1.2);
+    }
+    100% {
+        transform: scale(1,1);
+    }
 }
 </style>
