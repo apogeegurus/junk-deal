7fd5155880e3c54837eee9ba3c3b954bff94146a
Hovo Nazaryan <hovnazaryan98@gmail.com>
2020-02-02 15:54:34 +0400

    imagemin plugin

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+// vue.config.js
+
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const ImageminMozJpeg = require('imagemin-mozjpeg');
+
+module.exports = {
+    configureWebpack: {
+        plugins: [
+            new ImageminPlugin({
+                plugins: [
+                    ImageminMozJpeg({
+                        quality: 50,
+                        progressive: false
+                    })
+                ],
+                pngquant: {
+                    quality: '65-90',
+                    speed: 4
+                },
+                gifsicle: {
+                    interlaced: true,
+                    optimizationLevel: 3
+                },
+                svgo: {
+                    removeViewBox: false
+                }
+            })
+        ]
+    }
+}
