1e3f963c0476ab04019c7359ba7f0267485824ff
Hovo Nazaryan <hovnazaryan98@gmail.com>
2020-02-29 16:05:25 +0400

    changed header items

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -19,19 +19,13 @@
 
                     <section class="d-md-flex justify-content-between text-uppercase mt-2 flex-lg-row flex-md-column">
                         <b-nav-item :to="{ name: 'home'}" class="menu-item">Home</b-nav-item>
-                        <b-nav-item-dropdown text="About Us" right no-caret class="menu-item">
-                            <b-dropdown-item :to="{ name: 'blogs' }">Our Blogs</b-dropdown-item>
-                            <b-dropdown-item :to="{ name: 'locations' }">Locations</b-dropdown-item>
-                            <b-dropdown-item :to="{ name: 'about' }">About Junk Deal</b-dropdown-item>
-                            <b-dropdown-item href="#">Our Message</b-dropdown-item>
-                            <b-dropdown-item href="#">Who is who in Junk deal</b-dropdown-item>
-                            <b-dropdown-item href="#">Planing for the future</b-dropdown-item>
-                            <b-dropdown-item href="#">Tesimonials</b-dropdown-item>
-                        </b-nav-item-dropdown>
+                        <b-nav-item :to="{ name: 'about'}" class="menu-item">About</b-nav-item>
+                        <b-nav-item :to="{ name: 'blogs'}" class="menu-item">Blog</b-nav-item>
+                        <b-nav-item :to="{ name: 'locations'}" class="menu-item">Locations</b-nav-item>
                         <b-nav-item-dropdown text="Services" right no-caret class="menu-item">
                             <b-dropdown-item :to="{name: 'services', params: { slug: item.slug }}" v-for="(item, key) in SERVICES" :key="key">{{ item.title }}</b-dropdown-item>
                         </b-nav-item-dropdown>
-                        <b-nav-item :to="{ name: 'contact' }" class="menu-item">Contact Us</b-nav-item>
+                        <b-nav-item :to="{ name: 'contact' }" class="menu-item">Contact</b-nav-item>
                     </section>
                 </b-navbar-nav>
             </b-collapse>
