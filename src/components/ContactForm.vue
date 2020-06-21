7c682ea4c5388a8c993ce58ca34bc6c730512caf
hovnazaryan77 <hovnazaryan77@gmail.com>
2020-06-21 21:54:41 +0400

    small fix

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,25 +1,25 @@
 <template>
     <b-form>
         <b-form-group class="mb-3">
-            <b-input type="text" placeholder="Enter Name" class="jd-input"  v-validate="'required'" name="name" v-model="contact.name"></b-input>
+            <b-input type="text" placeholder="Name" class="jd-input"  v-validate="'required'" name="name" v-model="contact.name"></b-input>
             <span v-if="veeErrors.has('name')" class="text-danger jd-text-12">
                                         {{ veeErrors.first('name') }}
                                     </span>
         </b-form-group>
         <b-form-group class="mb-3">
-            <b-input type="email" placeholder="Enter Email" class="jd-input" v-validate="'required|email'" name="email" v-model="contact.email"></b-input>
+            <b-input type="email" placeholder="Email" class="jd-input" v-validate="'required|email'" name="email" v-model="contact.email"></b-input>
             <span v-if="veeErrors.has('email')" class="text-danger jd-text-12">
                                         {{ veeErrors.first('email') }}
                                     </span>
         </b-form-group>
         <b-form-group class="mb-3">
-            <b-input type="text" placeholder="Enter Subject" class="jd-input" v-validate="'required'" name="subject" v-model="contact.subject"></b-input>
+            <b-input type="text" placeholder="Subject" class="jd-input" v-validate="'required'" name="subject" v-model="contact.subject"></b-input>
             <span v-if="veeErrors.has('subject')" class="text-danger jd-text-12">
                                         {{ veeErrors.first('subject') }}
                                     </span>
         </b-form-group>
         <b-form-group class="mb-3">
-            <b-textarea placeholder="Enter Message" rows="4" class="jd-input" v-validate="'required'" name="message" v-model="contact.message"></b-textarea>
+            <b-textarea placeholder="Message" rows="4" class="jd-input" v-validate="'required'" name="message" v-model="contact.message"></b-textarea>
             <span v-if="veeErrors.has('message')" class="text-danger jd-text-12">
                                         {{ veeErrors.first('message') }}
                                     </span>
