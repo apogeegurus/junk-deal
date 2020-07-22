d47686c6c42a78354a567da8c17f4c23c62f93b9
hovnazaryan <hovnazaryan98@gmail.com>
2020-07-23 00:35:15 +0400

    location design changes

--- src/components/weatherSection.vue
+++ src/components/weatherSection.vue
@@ -1,53 +1,93 @@
 <template>
-    <div class="position-relative" style="z-index: 980">
-        <h2 class="jd-text-22 jd-font-medium bottom-line-def-color jd-text-light">Weather in <br> {{ location.city }}</h2>
+    <div class="position-relative" style="z-index: 980;margin: 0 -7px;">
+        <div class="text-center mb-20">
+            <img :src="location.city_emblem_path" alt="" title="" class="emblem" width="122px" height="122px"/>
+        </div>
+        <h2 class="jd-text-22 jd-font-medium bottom-line-def-color jd-text-light weather-text-color jd-font-medium">Weather in <br> {{ location.city }}</h2>
         <h3 class="jd-font-light d-flex justify-content-lg-between justify-content-start">
             <span style="font-size: 93px;font-weight: 200;" class="jd-font-ultra-light weather-text-style">{{ location.weather }}°</span>
             <b-img :src="`/img/weather/${location.weather_icon}.svg`" class="contain-img style-weather" v-if="location.weather_icon"></b-img>
         </h3>
 
-        <h2 class="jd-text-22 jd-font-medium bottom-line-def-color jd-text-light">Resources:</h2>
+        <h2 class="jd-text-22 jd-font-medium bottom-line-def-color jd-text-light weather-text-color">{{ location.city }} Stats</h2>
+        <ul class="d-flex flex-column list-info">
+            <li class="d-flex justify-content-between">
+                <p>Population</p>
+                <p>{{ location.population }}</p>
+            </li>
+            <li class="d-flex justify-content-between">
+                <p>Average Age</p>
+                <p>{{ location.average_age }}</p>
+            </li>
+            <li class="d-flex justify-content-between">
+                <p>Median Income</p>
+                <p>{{ location.median_income }}</p>
+            </li>
+            <li class="d-flex justify-content-between">
+                <p>Median Home Value</p>
+                <p>${{ location.median_home_value }}</p>
+            </li>
+            <li class="align-self-end">
+                <a :href="location.wiki_link" target="_blank">Learn More on Wikipedia</a>
+            </li>
+        </ul>
+        <h3 class="jd-text-18 jd-text-dark jd-font-medium">Palo Alto City Hall</h3>
         <section class="jd-text-18">
-            <section>
-                <p class="mb-0">City of {{ location.city }}</p>
-                <section>
-                    <b-img src="/img/icons/globus.png" class="contain-img mr-3"></b-img>
-                    <a :href="location.website" target="_blank" class="text-link">{{ location.website }}</a>
-                </section>
+            <section class="d-flex align-items-center">
+                <b-img src="/img/icons/globus.png" class="contain-img mr-3"></b-img>
+                <a :href="location.website" target="_blank" class="text-link">{{ location.website }}</a>
             </section>
-
-            <section class="mt-4">
-                <p class="mb-0">City Hall</p>
-                <section>
-                    <b-img src="/img/icons/phone.svg" class="contain-img mr-3"></b-img>
-                    <a :href="`tel:${location.city_phone}`" target="_blank" class="jd-text-dark">{{ location.city_phone }}</a>
-                </section>
-            </section>
-
-            <section class="mt-4">
-                <p class="mb-0">Police Department,</p>
-                <p class="mb-0">{{ location.police_address }}</p>
-                <section>
-                    <b-img src="/img/icons/phone.svg" class="contain-img mr-3"></b-img>
-                    <a :href="`tel:${location.police_phone}`" target="_blank" class="jd-text-dark">{{ location.police_phone }}</a>
-                </section>
-
-                <section class="mt-2">
-<!--                    <b-img src="/img/icons/phone.svg" class="contain-img mr-3"></b-img>-->
-                    <a :href="`mailto:${location.police_email}`" target="_blank" class="text-link">{{ location.police_email }}</a>
-                </section>
-            </section>
-        </section>
-
-        <h2 class="jd-text-22 jd-font-medium bottom-line-def-color mt-4 jd-text-light">Places to donate:</h2>
-
-        <section class="jd-text-18">
-            <p class="mb-0">{{ location.donate_address }}</p>
-            <section>
+            <section class="d-flex align-items-center mt-3">
                 <b-img src="/img/icons/phone.svg" class="contain-img mr-3"></b-img>
-                <a :href="`tel:${location.donate_phone}`" target="_blank" class="jd-text-dark">{{ location.donate_phone }}</a>
+                <a :href="`tel:${location.city_phone}`" target="_blank" class="jd-text-dark">{{ location.city_phone }}</a>
+            </section>
+            <section class="d-flex align-items-center mt-3">
+                <b-img src="/img/icons/phone.svg" class="contain-img mr-3"></b-img>
+                <a :href="`tel:${location.address}`" target="_blank" class="jd-text-dark">{{ location.address }}</a>
             </section>
         </section>
+
+<!--        <section class="jd-text-18">-->
+<!--            <section>-->
+<!--                <p class="mb-0">City of {{ location.city }}</p>-->
+<!--                <section>-->
+<!--                    <b-img src="/img/icons/globus.png" class="contain-img mr-3"></b-img>-->
+<!--                    <a :href="location.website" target="_blank" class="text-link">{{ location.website }}</a>-->
+<!--                </section>-->
+<!--            </section>-->
+
+<!--            <section class="mt-4">-->
+<!--                <p class="mb-0">City Hall</p>-->
+<!--                <section>-->
+<!--                    <b-img src="/img/icons/phone.svg" class="contain-img mr-3"></b-img>-->
+<!--                    <a :href="`tel:${location.city_phone}`" target="_blank" class="jd-text-dark">{{ location.city_phone }}</a>-->
+<!--                </section>-->
+<!--            </section>-->
+
+<!--            <section class="mt-4">-->
+<!--                <p class="mb-0">Police Department,</p>-->
+<!--                <p class="mb-0">{{ location.police_address }}</p>-->
+<!--                <section>-->
+<!--                    <b-img src="/img/icons/phone.svg" class="contain-img mr-3"></b-img>-->
+<!--                    <a :href="`tel:${location.police_phone}`" target="_blank" class="jd-text-dark">{{ location.police_phone }}</a>-->
+<!--                </section>-->
+
+<!--                <section class="mt-2">-->
+<!--&lt;!&ndash;                    <b-img src="/img/icons/phone.svg" class="contain-img mr-3"></b-img>&ndash;&gt;-->
+<!--                    <a :href="`mailto:${location.police_email}`" target="_blank" class="text-link">{{ location.police_email }}</a>-->
+<!--                </section>-->
+<!--            </section>-->
+<!--        </section>-->
+
+<!--        <h2 class="jd-text-22 jd-font-medium bottom-line-def-color mt-4 jd-text-light">Places to donate:</h2>-->
+
+<!--        <section class="jd-text-18">-->
+<!--            <p class="mb-0">{{ location.donate_address }}</p>-->
+<!--            <section>-->
+<!--                <b-img src="/img/icons/phone.svg" class="contain-img mr-3"></b-img>-->
+<!--                <a :href="`tel:${location.donate_phone}`" target="_blank" class="jd-text-dark">{{ location.donate_phone }}</a>-->
+<!--            </section>-->
+<!--        </section>-->
     </div>
 </template>
 
@@ -65,6 +105,37 @@
 </script>
 
 <style scoped lang="scss">
+    .weather-text-color {
+        color: #8e8e8e;
+        font-size: 22px;
+    }
+
+    .list-info {
+        list-style: none;
+        padding: 0;
+        margin-bottom: 11px;
+        li {
+            border-bottom:1px solid #4a4a4a;
+            padding: 3px 0;
+            &:last-child {
+                border: none;
+                a {
+                    color: #0091ff;
+                    font-size: 14px;
+                }
+            }
+            p {
+                font-size: 18px;
+                color: #4a4a4a;
+                margin: 0;
+            }
+        }
+    }
+
+    .mb-20 {
+        margin-bottom: 20px;
+    }
+
     .style-weather{
         height: 90px;
         margin-left: 15px
@@ -80,7 +151,7 @@
     }
     .bottom-line-def-color:after{
         border-radius: 0 150px 150px 0;
-        margin-left: -15px;
+        margin-left: -8px;
         margin-top: 10px;
         @media screen and (max-width:992px){
             margin-left: 0;
--- src/views/Location/Show.vue
+++ src/views/Location/Show.vue
@@ -3,7 +3,7 @@
         <slider :withoutHero="true" :images="sliderImages" v-if="sliderImages.length"
                 classes="slider--location"></slider>
 
-        <b-container class="p-0__mobile z-index-1000 position-relative mt-50px mt-mobile-0">
+        <b-container class="p-0__mobile z-index-1000 position-relative mt-mobile-0">
             <b-row>
                 <b-col cols="12" lg="9" class="text-center text-lg-left">
                     <h1 class="jd-text-36 jd-text-dark jd-text-23__mobile text-left px-4 p-lg-0">{{ location.title
@@ -24,10 +24,12 @@
         </b-container>
 
         <section class="d-flex flex-column">
-            <section class="order-lg-0 order-2 mt-4 mt-lg-0">
+            <section class="order-lg-2 order-2 mt-4 mt-lg-0">
                 <h2 class="jd-text-25 jd-font-bold jd-text-dark d-block d-lg-none text-center">Additional Services</h2>
                 <services :services="SERVICES"></services>
             </section>
+
+            <testimonials class="mt-5 order-lg-0 order-1"></testimonials>
 
             <div class="map-container order-lg-0 order-0" @click="clickedMap = true" @mouseleave="clickedMap = false"
                  :class="{ clicked : clickedMap }">
@@ -36,32 +38,166 @@
                         allowfullscreen></iframe>
             </div>
 
-            <gallery class="mt-5 order-lg-0 order-1" :images="location.gallery"></gallery>
-
-            <weather-section class="order-lg-0 order-4 d-block d-lg-none px-4 mt-0"
-                             :location="location"></weather-section>
-            <testimonials class="mt-5 order-lg-0 order-5"></testimonials>
-
-            <banner-right :only-image="true" :image="location.banner_first_path"
-                          class="order-lg-0 order-3 overflow-hidden"></banner-right>
-            <b-container class="order-lg-0 order-6">
-                <h2 class="jd-text-27 text-uppercase text-lg-center text-left jd-text-23__mobile">
-                    <span class="jd-text-dark jd-font-bold">Facts about</span>
-                    <br class="d-block d-lg-none">
+            <b-container class="order-lg-0 order-3">
+                <h2 class="jd-text-27 text-uppercase text-lg-center text-left jd-text-23__mobile mt-5 mb-5">
+                    <span class="jd-text-dark jd-font-bold">Places Around</span>
                     <span class="jd-theme-color jd-font-bold"> {{ location.city }}</span>
-                    <span class="brake-mobile"> You probably didn’t know</span>
+                    <span class="brake-mobile text--light"> You can find useful</span>
                 </h2>
+            </b-container>
 
-                <b-row class="mt-5 mb-5">
-                    <b-col cols="12" lg="4" class="jd-font-medium jd-text-18 jd-text-dark"
-                           v-html="location.facts_left"></b-col>
-                    <b-col cols="12" lg="7" offset="0" offset-lg="1"
-                           class="jd-font-medium jd-text-18 jd-text-light d-none d-lg-block"
-                           v-html="location.facts_right"></b-col>
+            <b-container class="order-lg-0 order-3">
+                <b-row>
+                    <b-col v-for="place in chunkedPlaces()[0]" cols="12" lg="3">
+                        <section class="jd-text-18 jd-font-medium mb-5 mb-lg-0">
+                            <section class="d-flex align-items-center">
+                                <b-img src="/img/icons/globus.png" class="contain-img mr-3"></b-img>
+                                <a :href="place.url" target="_blank" class="text-link">{{ place.name }}</a>
+                            </section>
+                            <section class="d-flex align-items-center mt-3">
+                                <b-img src="/img/icons/phone.svg" class="contain-img mr-3"></b-img>
+                                <a :href="`tel:${place.address}`" target="_blank" class="jd-text-dark">{{ place.address
+                                    }}</a>
+                            </section>
+                            <section class="d-flex align-items-center mt-3">
+                                <b-img src="/img/icons/phone.svg" class="contain-img mr-3"></b-img>
+                                <a :href="`tel:${place.phone}`" target="_blank" class="jd-text-dark">{{ place.phone
+                                    }}</a>
+                            </section>
+                        </section>
+                    </b-col>
                 </b-row>
             </b-container>
+
+            <weather-section class="order-lg-0 order--1 d-block d-lg-none px-4 mt-0"
+                             :location="location"></weather-section>
+
+            <banner-right :only-image="true" :image="location.banner_first_path"
+                          class="order-lg-0 order-2 overflow-hidden"></banner-right>
+
+            <b-container class="d-none d-lg-block">
+                <b-row>
+                    <b-col v-for="place in chunkedPlaces()[1]">
+                        <section class="jd-text-18 jd-font-medium">
+                            <section class="d-flex align-items-center">
+                                <b-img src="/img/icons/globus.png" class="contain-img mr-3"></b-img>
+                                <a :href="place.url" target="_blank" class="text-link">{{ place.name }}</a>
+                            </section>
+                            <section class="d-flex align-items-center mt-3">
+                                <b-img src="/img/icons/phone.svg" class="contain-img mr-3"></b-img>
+                                <a :href="`tel:${place.address}`" target="_blank" class="jd-text-dark">{{ place.address
+                                    }}</a>
+                            </section>
+                            <section class="d-flex align-items-center mt-3">
+                                <b-img src="/img/icons/phone.svg" class="contain-img mr-3"></b-img>
+                                <a :href="`tel:${place.phone}`" target="_blank" class="jd-text-dark">{{ place.phone
+                                    }}</a>
+                            </section>
+                        </section>
+                    </b-col>
+                </b-row>
+            </b-container>
+
+            <section class="order-lg-0 order-4">
+                <div class="headline jd-text-27 text-center my-5 text-uppercase">
+                    <b-container>
+                        <span class="jd-text-dark jd-font-bold">We clean </span>
+                        <span class="jd-theme-color jd-font-bold">YOUR PLACE </span>
+                        <span class="jd-text-dark jd-font-bold">AND SAVE </span>
+                        <span class="text-white jd-font-bold">YOUR TIME </span> <br>
+                        <span class="text-white jd-font-regular jd-text-20 opacity">SO HERE IS A COUPLE OF WAYS how TO PUT THIS TIME IN THE GOOD USE</span>
+                    </b-container>
+                </div>
+            </section>
+
+            <section class="order-lg-0 order-4">
+                <b-container>
+                    <h3 class="text-uppercase jd-text--22 light-headline jd-font-medium bottom-line-def-color mt--4">
+                        What to eat in <span class="jd-theme-color">{{ location.city }}</span></h3>
+
+                    <b-row>
+                        <b-col cols="12" lg="4">
+                            <!--                            <div class="" v-for="gallery in location.gallery">-->
+                            <!--                                <img src="/" alt="" title="" />-->
+                            <!--                            </div>-->
+                        </b-col>
+
+                        <b-col cols="12" lg="8" v-html="location.what_to_eat" style="line-height: 26px"></b-col>
+                    </b-row>
+
+                    <section class="d-none d-lg-block mb-5 mt-4">
+                        <b-row>
+                            <b-col v-for="yelp_place in yelpPlaces()[0]" class="yelp--item" >
+                                <a :href="yelp_place.url" target="_blank" class="jd-text-dark">
+                                    <h2 class="jd-font-bold">
+                                        {{ yelp_place.name }}
+                                    </h2>
+                                    <div>
+                                        <div>
+                                            <img :src="yelp_place.img" alt="" title=""/>
+                                        </div>
+                                        <div class="d-flex flex-column">
+                                            <p>{{ yelp_place.address }}</p>
+                                            <div class="d-flex">
+                                                <img :src="`/img/yelp/regular_${yelp_place.rating}.png`" alt="" title=""
+                                                     style="object-fit: contain;height: 20px;"/>
+                                                <img src="/img/yelp/logo.png" alt="" title=""
+                                                     style="object-fit: contain;height: 20px;width: 50px"/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </b-col>
+                        </b-row>
+                    </section>
+                </b-container>
+            </section>
+
+
             <banner-left :only-image="true" :image="location.banner_second_path"
                          class="mb-5 d-none d-lg-block"></banner-left>
+
+            <section class="order-lg-0 order-5">
+                <b-container>
+                    <h3 class="text-uppercase jd-text--22 light-headline jd-font-medium bottom-line-def-color mt--4">
+                        Where to go in <span class="jd-theme-color">{{ location.city }}</span></h3>
+
+                    <b-row>
+                        <b-col cols="12" lg="8" v-html="location.what_to_eat" style="line-height: 26px"></b-col>
+                        <b-col cols="12" lg="4">
+                            <!--                            <div class="" v-for="gallery in location.gallery">-->
+                            <!--                                <img src="/" alt="" title="" />-->
+                            <!--                            </div>-->
+                        </b-col>
+                    </b-row>
+
+                    <section class="d-none d-lg-block mb-5 mt-4">
+                        <b-row>
+                            <b-col v-for="yelp_place in yelpPlaces()[1]" class="yelp--item">
+                                <a :href="yelp_place.url" target="_blank" class="jd-text-dark">
+                                    <h2 class="jd-font-bold">
+                                        {{ yelp_place.name }}
+                                    </h2>
+                                    <div>
+                                        <div>
+                                            <img :src="yelp_place.img" alt="" title=""/>
+                                        </div>
+                                        <div class="d-flex flex-column">
+                                            <p>{{ yelp_place.address }}</p>
+                                            <div class="d-flex">
+                                                <img :src="`/img/yelp/regular_${yelp_place.rating}.png`" alt="" title=""
+                                                     style="object-fit: contain;height: 20px;"/>
+                                                <img src="/img/yelp/logo.png" alt="" title=""
+                                                     style="object-fit: contain;height: 20px;width: 50px"/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </b-col>
+                        </b-row>
+                    </section>
+                </b-container>
+            </section>
         </section>
     </div>
 </template>
@@ -75,7 +211,7 @@
     import Slider from "./../_partials/Home/Slider";
     import Specialize from "./../_partials/Home/Specialize";
     import WeatherSection from "./../../components/weatherSection";
-    import {RepositoryFactory} from "./../../api/RepositoryFactory"
+    import {RepositoryFactory} from "../../api/RepositoryFactory"
     import {mapGetters} from "vuex";
 
     export default {
@@ -98,6 +234,26 @@
                 this.location = location;
                 this.$root.$emit('hideLoader');
             });
+        },
+        methods: {
+            chunkedPlaces: function () {
+                if (!this.location.places || !this.location.places.length) return [[], []];
+
+                const places = JSON.parse(JSON.stringify(this.location.places));
+                return [
+                    places.slice(0, 3),
+                    places.slice(3, 6)
+                ]
+            },
+            yelpPlaces() {
+                if (!this.location.yelp_places || !this.location.yelp_places.length) return [[], []];
+
+                const places = JSON.parse(JSON.stringify(this.location.yelp_places));
+                return [
+                    places.slice(0, 3),
+                    places.slice(3, 6)
+                ]
+            }
         },
         computed: {
             ...mapGetters(['SERVICES', "SPECIALIZES"]),
@@ -131,7 +287,7 @@
             margin-top: 10px !important;
         }
 
-        .brake-mobile{
+        .brake-mobile {
             display: block;
         }
     }
@@ -144,6 +300,50 @@
         .p-0__mobile {
             padding: 0;
         }
+    }
+
+    .opacity {
+        color: #dedede;
+    }
+
+    .light-headline {
+        color: #8e8e8e;
+    }
+
+
+    .order--1 {
+        order: -1;
+    }
+
+    .yelp--item {
+        display: flex;
+        justify-content: space-between;
+        flex-direction: column;
+
+        h2 {
+            text-align: center;
+            color: #4a4a4a;
+            font-size: 18px;
+        }
+
+        div {
+            display: flex;
+
+            img {
+                width: 100px;
+                height: 80px;
+                object-fit: cover;
+                border-radius: 5px;
+                margin-right: 20px;
+            }
+        }
+    }
+
+    .headline {
+        background: #a0a0a0;
+        padding: 20px 0;
+        line-height: 32px;
+        letter-spacing: 1.1px;
     }
 
     .b-l-1 {
@@ -167,5 +367,14 @@
 
     .mt-50px {
         margin-top: 50px;
+    }
+
+    .text--light {
+        color: #a0a0a0;
+        font-weight: 100;
+    }
+
+    .text-link {
+        color: #0091ff;
     }
 </style>
